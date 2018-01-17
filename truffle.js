ad1e94f20db6f42c42315918911bee6fa662691d
teleport-palis <cpalis@teleport.earth>
2018-01-17 23:13:40 +0800

    update

--- truffle.js
+++ truffle.js
@@ -9,10 +9,12 @@ module.exports = {
     ropsten: {
       network_id: 3,
       host: 'localhost',
+      //host: '76.178.24.27',
       port: 8545,
       gas: 4700000,
       gasPrice: 22000000000,
-      from: '0x30a259900656F599EDEEBF1eB7E1fBf948072Ba3'
+      // from: '0x30a259900656F599EDEEBF1eB7E1fBf948072Ba3',
+      // provider: new Web3.providers.HttpProvider('http://76.178.24.27:8545')
     },
     development: {
       host: "localhost",
